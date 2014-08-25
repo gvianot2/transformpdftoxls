1c3622371d8a732c0ca872463c59dbc2db947900
Vianot guillaume <gvianot@yahoo.com>
2014-08-25 10:07:32 +0200

    Create extractArraysCsv.rb

---
+++ extractArraysCsv.rb
@@ -0,0 +1,27 @@
+require 'tabula'
+require 'axlsx'
+require 'csv'
+
+pdf_dir_path = "/home/giom/Bureau/Merlu/tabula/"
+pdf_file_name = "datatables_sampleall.pdf"
+
+p = Axlsx::Package.new
+wb = p.workbook
+
+extractor = Tabula::Extraction::ObjectExtractor.new(pdf_dir_path + pdf_file_name, :all )
+i = 0
+
+extractor.extract.each do |pdf_page|
+  pdf_page.spreadsheets.each do |spreadsheet|
+    
+    wb.add_worksheet(:name => "tableau" + i.to_s) do | sheet |
+     spreadsheet.rows.each do |currentrow|
+	sheet.add_row currentrow.map(&:text)
+     end
+   end
+    i +=1
+    
+  end
+
+end
+p.serialize pdf_file_name+'.xlsx'
